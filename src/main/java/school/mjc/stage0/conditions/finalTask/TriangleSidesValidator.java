919d458b72711b6bc1c656088c1d7eadbec25b6b
rgsad <rsad3262@gmail.com>
2023-02-12 00:43:48 +0300

    refactor tasks

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,7 +2,9 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        if (firstSide + secondSide > thirdSide) {
+        if (firstSide < 1 || secondSide < 1 || thirdSide < 1) {
+            System.out.println("it's not a triangle");
+        } else if (firstSide + secondSide > thirdSide) {
             System.out.println("this is a valid triangle");
         } else if (firstSide + thirdSide > secondSide) {
             System.out.println("this is a valid triangle");
