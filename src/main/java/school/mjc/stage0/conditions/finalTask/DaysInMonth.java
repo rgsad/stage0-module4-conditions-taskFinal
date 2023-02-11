e7288e1ab3a7dab9aa5bb4c030bc48d0eaedecf9
rgsad <rsad3262@gmail.com>
2023-02-12 00:19:00 +0300

    start tasks

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,30 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+        }
+
+        boolean leap = year % 4 == 0 && year % 100 != 0 || year % 100 == 0 && year % 400 == 0;
+
+        if (leap && month  == 2) {
+            System.out.println(29);
+        }
+
+        switch (month) {
+            case 2:
+                System.out.println(28);
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
+            default:
+                System.out.println(31);
+                break;
+        }
 
     }
 }
