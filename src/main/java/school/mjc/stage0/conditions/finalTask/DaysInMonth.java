4a9745425c5750ee854af7fc1ebeb038fd783fc2
rgsad <rsad3262@gmail.com>
2023-02-12 01:01:53 +0300

    refactor TriangleSidesValidator task && DaysInMonth task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,20 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-        if (year < 0) {
-            System.out.println("invalid date");
-        }
-
         boolean leap = year % 4 == 0 && year % 100 != 0 || year % 100 == 0 && year % 400 == 0;
-
-        if (year > 0 && leap && month  == 2) {
+        if (year < 0 || month < 0 || month > 12) {
+            System.out.println("invalid date");
+        } else if (leap && month == 2) {
             System.out.println(29);
         } else {
             switch (month) {
                 case 2 -> System.out.println(28);
                 case 4, 6, 9, 11 -> System.out.println(30);
-                case 1, 3, 5, 7, 8, 10, 12 -> System.out.println(31);
-                default -> System.out.println("invalid date");
+                default -> System.out.println(31);
             }
         }
 
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,17 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        if (firstSide < 1 || secondSide < 1 || thirdSide < 1) {
-            System.out.println("it's not a triangle");
-        } else if (firstSide + secondSide > thirdSide) {
-            System.out.println("this is a valid triangle");
-        } else if (firstSide + thirdSide > secondSide) {
-            System.out.println("this is a valid triangle");
-        } else if (secondSide + thirdSide > firstSide) {
-            System.out.println("this is a valid triangle");
-        } else {
-            System.out.println("it's not a triangle");
-        }
+        boolean first = firstSide + secondSide > thirdSide;
+        boolean second = secondSide + thirdSide > firstSide;
+        boolean third = firstSide + thirdSide > secondSide;
 
+       if (first && second && third) {
+           System.out.println("this is a valid triangle");
+       } else {
+           System.out.println("it's not a triangle");
+       }
     }
 }
